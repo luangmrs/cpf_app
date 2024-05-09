890d268e8e9545a24fafcef0aee4457e146769f4
Luan <luangmrs@gmail.com>
2024-05-08 22:10:23 -0300

    Função gerarcpf criada.

--- cpf_app.c
+++ cpf_app.c
@@ -2,8 +2,16 @@
 #include <stdlib.h>
 #include <time.h>
 
-
+void gerarcpf(){
+    srand(time(NULL));
+    int digitoscpf[10];
+    for(int i = 0; i < 9; i++){ // 9 primeiros digitos
+        digitoscpf[i] = rand() % 9 + 1;
+        printf("%d", digitoscpf[i]);
+    }
+}
 
 int main(){
+    gerarcpf();
     return 0;
 }
