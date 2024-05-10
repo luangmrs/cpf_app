643b1c25969ff2a26a2d60bac6c45f6b54f83069
Luan <luangmrs@gmail.com>
2024-05-10 18:49:36 -0300

    Modificada lógica da função gerarcpf.

--- cpf_app.c
+++ cpf_app.c
@@ -2,26 +2,39 @@
 #include <stdlib.h>
 #include <time.h>
 
-void gerarcpf(){
-    srand(time(NULL));
-    int digitoscpf[10];
-    int decimodigito;
-    int somadigitos = 0;
-    for(int i = 0; i < 9; i++){ // 9 primeiros digitos
-        digitoscpf[i] = rand() % 9 + 1;
-        printf("%d", digitoscpf[i]);
-    }
-    for(int i = 0; i < 9; i++){
-        int m = 10;
-        somadigitos = digitoscpf[i * m] + somadigitos;
-        m--;
-    }
-    decimodigito = 11 - (somadigitos % 11);
-    printf("%d", decimodigito);
-    
-}
+void gerarcpf(int n){ // n = quantidade CPFS pra serem gerados.
+    for(int i = 0; i < n; i++){
+        int digitoscpf[11];
+        int resto, soma = 0;
+        
+        for(int i = 0; i < 9; i++){ // 9 primeiros digitos
+            digitoscpf[i] = rand() % 10;
+        }
+        for(int i = 0; i < 9; i++){
+            soma += digitoscpf[i] * (10-i);
+        }
+        // primeiro digito verificador.
+        resto = soma % 11;
+        digitoscpf[9] = (resto < 2) ? 0 : (11 - resto);
+        
+        soma = 0;
+        for(int i = 0; i < 10; i++){
+            soma += digitoscpf[i] * (11-i);
+        }
+        // segundo digito verificador.
+        resto = soma % 11;
+        digitoscpf[10] = (resto < 2) ? 0 : (11 - resto);
+
+        for(int i = 0; i < 11; i++){
+            printf("%d", digitoscpf[i]);
+        }
+    printf("\n");
+}}
 
 int main(){
-    gerarcpf();
+    srand(time(NULL));
+    
+    gerarcpf(0);
+    
     return 0;
 }
