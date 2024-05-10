2975009805b42ff38be7e1832ed8207912edf677
Luan <luangmrs@gmail.com>
2024-05-09 23:17:18 -0300

    WIP

--- cpf_app.c
+++ cpf_app.c
@@ -5,10 +5,20 @@
 void gerarcpf(){
     srand(time(NULL));
     int digitoscpf[10];
+    int decimodigito;
+    int somadigitos = 0;
     for(int i = 0; i < 9; i++){ // 9 primeiros digitos
         digitoscpf[i] = rand() % 9 + 1;
         printf("%d", digitoscpf[i]);
     }
+    for(int i = 0; i < 9; i++){
+        int m = 10;
+        somadigitos = digitoscpf[i * m] + somadigitos;
+        m--;
+    }
+    decimodigito = 11 - (somadigitos % 11);
+    printf("%d", decimodigito);
+    
 }
 
 int main(){
