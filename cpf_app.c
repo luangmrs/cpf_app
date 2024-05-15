ea2a9cb60c0d855a1ae004e434aac63aa5a31878
Luan <luangmrs@gmail.com>
2024-05-15 20:41:18 -0300

    Alterada lógica da função gerarCPF para conseguir gravar as saídas em arquivo. Case 1 do switch feito.

--- cpf_app.c
+++ cpf_app.c
@@ -2,47 +2,46 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <conio.h>
 
-void gerarcpf(int n){ // n = quantidade CPFS pra serem gerados.
-    for(int i = 0; i < n; i++){
-        
-        int digitoscpf[11];
-        int resto, soma = 0;
-        
-        for(int i = 0; i < 9; i++){ // 9 primeiros digitos
-            digitoscpf[i] = rand() % 10;
-        }
-        for(int i = 0; i < 9; i++){
-            soma += digitoscpf[i] * (10-i);
-        }
-        // primeiro digito verificador.
-        resto = soma % 11;
-        digitoscpf[9] = (resto < 2) ? 0 : (11 - resto);
-        
-        soma = 0;
-        for(int i = 0; i < 10; i++){
-            soma += digitoscpf[i] * (11-i);
-        }
-        // segundo digito verificador.
-        resto = soma % 11;
-        digitoscpf[10] = (resto < 2) ? 0 : (11 - resto);
+void gerarcpf(char* cpf){ // n = quantidade CPFS pra serem gerados.
+    int digitoscpf[11];
+    int resto, soma = 0;
 
+    for(int i = 0; i < 9; i++){ // 9 primeiros digitos
+        digitoscpf[i] = rand() % 10;
+    }
+    for(int i = 0; i < 9; i++){
+        soma += digitoscpf[i] * (10-i);
+    }
+    // Primeiro digito verificador.
+    resto = soma % 11;
+    digitoscpf[9] = (resto < 2) ? 0 : (11 - resto);
+
+    soma = 0;
+    for(int i = 0; i < 10; i++){
+        soma += digitoscpf[i] * (11-i);
+    }
+    // Segundo digito verificador.
+    resto = soma % 11;
+    digitoscpf[10] = (resto < 2) ? 0 : (11 - resto);
+
+    for(int i = 0; i < 11; i++){ // Formatando como string;
+        cpf[i] = digitoscpf[i] + '0';
+    }
+    cpf[11] = '\0';
     
-        for(int i = 0; i < 11; i++){
-            printf("%d", digitoscpf[i]);
-        }
-    printf("\n");
-}}
+}
 
 int validarCPF(const char cpf[]){
 
     int dv1, dv2, resto = 0, soma = 0;
 
-    if(strlen(cpf) != 11){//validação do tamanho
+    if(strlen(cpf) != 11){// Validação do tamanho
         return 0;
     }
 
-    for(int i = 0; i < 9; i++){ //validação primeiro digito verificador
+    for(int i = 0; i < 9; i++){ // Validação primeiro digito verificador
         soma+= (cpf[i]-'0')*(10-i);
     }
     resto = (soma % 11) < 2? 0 : 11-(soma%11);
@@ -53,7 +52,7 @@ int validarCPF(const char cpf[]){
 
     soma = 0;
     resto = 0;
-    for(int i = 0; i < 10; i++){//validação segundo digito verificador
+    for(int i = 0; i < 10; i++){// Validação segundo digito verificador
         soma+= (cpf[i]-'0')*(11-i);
     }
     resto = (soma % 11) < 2? 0 : 11-(soma%11);
@@ -69,6 +68,7 @@ int main(){
     srand(time(NULL));
     char opcao, cpf[12];
     int qtdcpfs;
+    FILE *arq;
 
     printf("Seja bem vindo ao CPF APP!\n");
     printf("Atencao as observacoes:\n(1) - Os CPFs gerados sairao no arquivo 'output.txt'.\n");
@@ -76,22 +76,33 @@ int main(){
     printf("(3) - Os CPFs a serem validados devem ser apenas numeros, sem caracteres.\n");
     printf("Pressione enter para continuar...");
     getchar();
-    system("cls");
     
-    printf("Escolha a opcao desejada:\n");
-    printf("1 - Gerador de CPF.\n");
+    
+    
+    printf("\n1 - Gerador de CPF.\n");
     printf("2 - Validador de CPF.\n");
-    printf(": ");
+    printf("Escolha a opcao desejada: ");
     scanf("%c", &opcao);
     getchar();
+    
 
     switch (opcao)
     {
     case '1':
-        printf("Digite quantos CPFs a serem gerados: ");
+        printf("\nDigite quantos CPFs a serem gerados: ");
         scanf("%d", &qtdcpfs);
-        gerarcpf(qtdcpfs);
+        arq = fopen("output.txt", "w");
+        if(arq == NULL){
+            printf("Problema ao criar o arquivo.");
+            return 1;
+        }
+         for (int i = 0; i < qtdcpfs; i++){
+            gerarcpf(cpf);
+            fprintf(arq, "%s\n", cpf);
+        }
+        printf("%d CPFs gerados e salvos em 'output.txt'.\n", qtdcpfs);
         break;
+
     case '2':
         printf("Digite o CPF a ser validado: ");
         scanf("%11s", cpf);
@@ -106,6 +117,7 @@ int main(){
         printf("Opcao invalida.");
         break;
     }
-
+    printf("\nPressione enter para fechar o programa.");
+    getch();
     return 0;
 }
